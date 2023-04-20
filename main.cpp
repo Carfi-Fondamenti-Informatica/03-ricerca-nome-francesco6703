76a1a3ed40809ad607dd988926643e070b7f6b2f
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-04-20 09:11:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main(){
+	int i, j, z;
+    char lista_nomi[3][5];
+    char nome[5];
+    cin >> lista_nomi[0];
+    cin >> lista_nomi[1];
+    cin >> lista_nomi[2];
+    cout << endl;
+    cout << endl;
+    cin >> nome;
+	
+	
+		for(i=0; lista_nomi[0][i]==nome[i]; i++)
+		{
+			if(i=5){
+			cout << "0";
+		}
+		}
+		
+		for(j=0; lista_nomi[1][j]==nome[j]; j++)
+		{
+			if(j=5){
+			cout << "1";
+		}
+		}
+		
+		for(z=0; lista_nomi[2][z]==nome[z]; z++)
+		{
+			if(z=5){
+			cout << "2";
+		}
+		}
+		
+		cout << "";
+	return 0;
 }
